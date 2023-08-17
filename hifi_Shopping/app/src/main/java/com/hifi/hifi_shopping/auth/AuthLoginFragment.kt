b27ec7d4c32ff297c9dba5fe77b7932871fcd508
KHSSE <113084857+KHSSE@users.noreply.github.com>
2023-08-17 23:54:21 +0900

    design : 회원가입 화면 이동 추가

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/auth/AuthLoginFragment.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/auth/AuthLoginFragment.kt
@@ -12,14 +12,26 @@ import com.hifi.hifi_shopping.databinding.FragmentAuthLoginBinding
 class AuthLoginFragment : Fragment() {
 
     lateinit var fragmentAuthLoginBinding: FragmentAuthLoginBinding
+    lateinit var authActivity : AuthActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        fragmentAuthLoginBinding = FragmentAuthLoginBinding.inflate(inflater)
 
+        fragmentAuthLoginBinding = FragmentAuthLoginBinding.inflate(inflater)
+        authActivity = activity as AuthActivity
+
+        fragmentAuthLoginBinding.run{
+
+            // 회원가입 텍스트 클릭 => LoginFragment로 교체
+            textViewAuthJoin.run{
+                setOnClickListener{
+                    authActivity.replaceFragment(AuthActivity.AUTH_JOIN_FRAGMENT, true, null)
+                }
+            }
+        }
 
         return fragmentAuthLoginBinding.root
     }
