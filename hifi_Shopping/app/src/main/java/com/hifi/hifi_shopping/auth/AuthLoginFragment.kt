b08c36c1d08b212c2c7c678f9f3c6cb3d963ac58
KHSSE <113084857+KHSSE@users.noreply.github.com>
2023-08-17 16:18:44 +0900

    feat : AuthMainActivity 추가

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/auth/AuthLoginFragment.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/auth/AuthLoginFragment.kt
@@ -20,17 +20,6 @@ class AuthLoginFragment : Fragment() {
         // Inflate the layout for this fragment
         fragmentAuthLoginBinding = FragmentAuthLoginBinding.inflate(inflater)
 
-        // MaterialCheckBox 클릭 리스너 등록
-        fragmentAuthLoginBinding.CheckBox.setOnCheckedChangeListener { checkBox, isChecked ->
-            // isChecked 값이 true일 경우, 체크가 되었을 때 실행되는 코드
-            if (isChecked) {
-                // 체크가 되었을 때 처리할 코드 작성
-                fragmentAuthLoginBinding.textViewTest.text = "체크되었습니다."
-            } else {
-                // 체크가 해제되었을 때 처리할 코드 작성
-                fragmentAuthLoginBinding.textViewTest.text = "체크가 해제되었습니다."
-            }
-        }
 
         return fragmentAuthLoginBinding.root
     }
