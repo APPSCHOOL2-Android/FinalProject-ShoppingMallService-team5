d2ec29f06cfa50f2fbfe3f340237db85eb857eda
KHSSE <113084857+KHSSE@users.noreply.github.com>
2023-08-18 00:00:46 +0900

    feat : 네비게이션 아이콘 동작 설정

---
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/auth/AuthJoinFragment.kt
@@ -0,0 +1,35 @@
+package com.hifi.hifi_shopping.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hifi.hifi_shopping.R
+import com.hifi.hifi_shopping.databinding.FragmentAuthJoinBinding
+import com.hifi.hifi_shopping.databinding.FragmentAuthLoginBinding
+
+
+class AuthJoinFragment : Fragment() {
+
+    lateinit var fragmentAuthJoinBinding: FragmentAuthJoinBinding
+    lateinit var authActivity: AuthActivity
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        fragmentAuthJoinBinding = FragmentAuthJoinBinding.inflate(inflater)
+        authActivity = activity as AuthActivity
+
+        fragmentAuthJoinBinding.run {
+            // 네비게이션 아이콘을 클릭했을 때의 동작 설정
+            toolbarAuthJoin.setNavigationOnClickListener {
+                authActivity.onBackPressed()
+            }
+        }
+
+        return fragmentAuthJoinBinding.root
+    }
+}
