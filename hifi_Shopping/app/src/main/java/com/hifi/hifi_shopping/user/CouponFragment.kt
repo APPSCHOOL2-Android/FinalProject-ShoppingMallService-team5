4f7663096257d402c72156781b8d1be2be70c48d
iiolo <alohalo98@naver.com>
2023-08-17 23:04:57 +0900

    feat : 쿠폰 화면 큰 틀 완성

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/CouponFragment.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/CouponFragment.kt
@@ -5,56 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.hifi.hifi_shopping.R
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.hifi.hifi_shopping.MainActivity
+import com.hifi.hifi_shopping.databinding.FragmentCouponBinding
+import com.hifi.hifi_shopping.user.adapter.CouponViewPagerAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CouponFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CouponFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var fragmentCouponBinding: FragmentCouponBinding
+    lateinit var mainActivity: MainActivity
+
+    private val tabTextList = listOf("보유 중인 쿠폰","미보유 중인 쿠폰")
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager2
+    private lateinit var adapter: CouponViewPagerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_coupon, container, false)
+        fragmentCouponBinding = FragmentCouponBinding.inflate(layoutInflater)
+        mainActivity = activity as MainActivity
+
+        fragmentCouponBinding.run {
+            tabLayout = couponTabLayout
+            viewPager = couponViewPager
+
+            // adapter 준비 및 연결
+            adapter = CouponViewPagerAdapter(this@CouponFragment)
+            viewPager.adapter = adapter
+
+        }
+
+        // TabLayout, ViewPager 연결
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = tabTextList[0]
+                else -> tab.text = tabTextList[1]
+            }
+        }.attach()
+
+        return fragmentCouponBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CouponFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CouponFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
