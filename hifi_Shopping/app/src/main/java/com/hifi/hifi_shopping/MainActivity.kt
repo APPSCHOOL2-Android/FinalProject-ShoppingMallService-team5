d3d76ed8b275793babff8d31569066cd74e0a774
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-08-18 10:52:39 +0900

    feat : 배송관리 화면 큰 틀 완성

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/MainActivity.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hifi.hifi_shopping.databinding.ActivityMainBinding
+import com.hifi.hifi_shopping.pacel.PacelActivity
 import com.hifi.hifi_shopping.review.ReviewActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
 
-        val intent = Intent(this@MainActivity, ReviewActivity::class.java)
+        val intent = Intent(this@MainActivity, PacelActivity::class.java)
         startActivity(intent)
 
     }
