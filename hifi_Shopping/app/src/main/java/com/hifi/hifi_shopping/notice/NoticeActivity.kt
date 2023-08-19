a860bb2e662a953ecc59df43c63e1a7b3a6f1056
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-08-19 21:35:45 +0900

    feat : 알림 리사이클러 뷰 아이템 적용

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/notice/NoticeActivity.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/notice/NoticeActivity.kt
@@ -3,10 +3,16 @@ package com.hifi.hifi_shopping.notice
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.core.graphics.drawable.toDrawable
 import androidx.core.graphics.toColor
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.google.android.material.divider.MaterialDividerItemDecoration
 import com.hifi.hifi_shopping.R
 import com.hifi.hifi_shopping.databinding.ActivityNoticeBinding
+import com.hifi.hifi_shopping.databinding.NoticeRecycItemBinding
 
 class NoticeActivity : AppCompatActivity() {
 
@@ -16,11 +22,27 @@ class NoticeActivity : AppCompatActivity() {
 
         uiSetting()
     }
-    fun uiSetting(){
-
+    private fun uiSetting(){
         activityNoticeBinding = ActivityNoticeBinding.inflate(layoutInflater)
         setContentView(activityNoticeBinding.root)
 
+        toolbarSetting()
+        buttonSetting()
+        recyclerViewSetting()
+
+    }
+
+    private fun recyclerViewSetting(){
+        activityNoticeBinding.run{
+            noticeRecyclerView.run{
+                adapter = NoticeAdapter()
+                layoutManager = LinearLayoutManager(context)
+                addItemDecoration(MaterialDividerItemDecoration(context, MaterialDividerItemDecoration.VERTICAL))
+            }
+        }
+    }
+
+    private fun toolbarSetting(){
         activityNoticeBinding.run{
             noticeMaterialToolbar.run{
                 title = "알림"
@@ -28,8 +50,12 @@ class NoticeActivity : AppCompatActivity() {
                 inflateMenu(R.menu.toolbar_menu_basic)
                 isTitleCentered = true
             }
+        }
+    }
 
-            noticeAllButton.run{
+    private fun buttonSetting(){
+        activityNoticeBinding.run {
+            noticeAllButton.run {
                 setOnClickListener {
                     noticeAllButton.setBackgroundResource(R.drawable.custom_select_button)
                     noticeAllButton.setTextColor(Color.WHITE)
@@ -42,7 +68,7 @@ class NoticeActivity : AppCompatActivity() {
                 }
             }
 
-            noticeOrderPareceButton.run{
+            noticeOrderPareceButton.run {
                 setOnClickListener {
                     noticeAllButton.setBackgroundResource(R.drawable.custom_normal_button)
                     noticeAllButton.setTextColor(getColor(R.color.lstButtonTextGrayColor))
@@ -55,7 +81,7 @@ class NoticeActivity : AppCompatActivity() {
                 }
             }
 
-            noticeSubscribeButton.run{
+            noticeSubscribeButton.run {
                 setOnClickListener {
                     noticeAllButton.setBackgroundResource(R.drawable.custom_normal_button)
                     noticeAllButton.setTextColor(getColor(R.color.lstButtonTextGrayColor))
@@ -68,7 +94,7 @@ class NoticeActivity : AppCompatActivity() {
                 }
             }
 
-            noticeWishListButton.run{
+            noticeWishListButton.run {
                 setOnClickListener {
                     noticeAllButton.setBackgroundResource(R.drawable.custom_normal_button)
                     noticeAllButton.setTextColor(getColor(R.color.lstButtonTextGrayColor))
@@ -80,7 +106,46 @@ class NoticeActivity : AppCompatActivity() {
                     noticeWishListButton.setTextColor(Color.WHITE)
                 }
             }
-
         }
     }
+
+    inner class NoticeAdapter: RecyclerView.Adapter<NoticeAdapter.NoticeViewHolder>() {
+        inner class NoticeViewHolder(noticeRecycItemBinding: NoticeRecycItemBinding) : ViewHolder(noticeRecycItemBinding.root){
+            var noticeTitleImageView = noticeRecycItemBinding.noticeTitleImageView
+            var noticeCheckImageView = noticeRecycItemBinding.noticeCheckImageView
+            var noticeTitleTextView = noticeRecycItemBinding.noticeTitleTextView
+            var noticeContentTextView = noticeRecycItemBinding.noticeContentTextView
+            var noticeDetailedImageView = noticeRecycItemBinding.noticeDetailedImageView
+            var noticeCategoryButton = noticeRecycItemBinding.noticeCategoryButton
+            var noticeDateTextView = noticeRecycItemBinding.noticeDateTextView
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoticeViewHolder {
+            val noticeRecycItemBinding = NoticeRecycItemBinding.inflate(layoutInflater)
+            val noticeViewHolder = NoticeViewHolder(noticeRecycItemBinding)
+
+            noticeRecycItemBinding.root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            return noticeViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 5
+        }
+
+        override fun onBindViewHolder(holder: NoticeViewHolder, position: Int) {
+            holder.noticeTitleImageView.setImageResource(R.drawable.hobby2)
+            holder.noticeCheckImageView.setBackgroundResource(R.drawable.item_notice_uncheck)
+            holder.noticeTitleTextView.text = "알림 제목"
+            holder.noticeContentTextView.text = "알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용"
+            holder.noticeDetailedImageView.setImageResource(R.drawable.chevron_right_24px)
+            holder.noticeCategoryButton.text = "알림 카테고리"
+            holder.noticeDateTextView.text = "알림 발생 날짜"
+        }
+    }
+
 }
