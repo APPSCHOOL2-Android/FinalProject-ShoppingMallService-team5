f0fd2356652f11fd3f640d2e95e8119948cd58e9
iiolo <alohalo98@naver.com>
2023-08-22 15:33:11 +0900

    feat : user 저장, 정보 가져오는 메서드 추가

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/UserActivity.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/UserActivity.kt
@@ -3,11 +3,15 @@ package com.hifi.hifi_shopping.user
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.transition.MaterialSharedAxis
+import com.google.firebase.database.FirebaseDatabase
 import com.hifi.hifi_shopping.R
 import com.hifi.hifi_shopping.databinding.ActivityUserBinding
+import com.hifi.hifi_shopping.user.model.UserDataClass
+import java.util.UUID
 
 class UserActivity : AppCompatActivity() {
 
@@ -15,6 +19,8 @@ class UserActivity : AppCompatActivity() {
 
     var newFragment:Fragment? = null
     var oldFragment:Fragment? = null
+
+    lateinit var userData : UserDataClass
 
     companion object{
         val MY_PAGE_FRAGMENT = "MyPageFragment"
@@ -29,6 +35,7 @@ class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+        getUserData()
     }
 
     // 지정한 Fragment를 보여주는 메서드
@@ -98,5 +105,38 @@ class UserActivity : AppCompatActivity() {
     // Fragment를 BackStack에서 제거한다.
     fun removeFragment(name:String){
         supportFragmentManager.popBackStack(name, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+    }
+
+    fun addUserData(){
+        val uuid = UUID.randomUUID()
+        val idx = uuid.toString()
+
+        val userData = UserDataClass(idx,"ohsso98@naver.com", "0618","김대박",false,"01000000000","user_sample.jpg")
+
+        val database = FirebaseDatabase.getInstance()
+        val userDataRef = database.getReference("UserData")
+        userDataRef.push().setValue(userData).addOnCompleteListener{
+            Log.d("유저 데이터",userData.toString())
+        }
+    }
+
+    fun getUserData(){
+        val database = FirebaseDatabase.getInstance()
+        val userDataRef = database.getReference("UserData")
+        val loginUserId = "ohsso98@naver.com"
+
+        userDataRef.orderByChild("userId").equalTo(loginUserId).get().addOnCompleteListener {
+            for(c1 in it.result.children) {
+                val idx = c1.child("idx").value as String
+                val email = c1.child("email").value as String
+                val pw = c1.child("pw").value as String
+                val nickname = c1.child("nickname").value as String
+                val isVerify = c1.child("verify").value as Boolean
+                val phoneNum = c1.child("phoneNum").value as String
+                val profileImg = c1.child("profileImg").value as String
+
+                userData = UserDataClass(idx,email, pw, nickname, isVerify, phoneNum, profileImg)
+            }
+        }
     }
 }
