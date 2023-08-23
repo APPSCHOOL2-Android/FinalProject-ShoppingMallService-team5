86868657182fd6d589da9ae9b99f46a5399dc386
iiolo <alohalo98@naver.com>
2023-08-23 18:41:45 +0900

    fix : 팔로워인 유저 목록 안받아오는 문제 해결

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/vm/SubscribeViewModel.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/vm/SubscribeViewModel.kt
@@ -1,5 +1,6 @@
 package com.hifi.hifi_shopping.user.vm
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hifi.hifi_shopping.user.model.CouponDataClass
@@ -13,75 +14,92 @@ import com.hifi.hifi_shopping.user.repository.UserRepository
 
 class SubscribeViewModel : ViewModel() {
 
+    var subscribeList = MutableLiveData<MutableList<SubscribeDataClass>>()
     var followerList = MutableLiveData<MutableList<UserDataClass>>()
     var followingList = MutableLiveData<MutableList<UserDataClass>>()
 
-    fun getFollowerAll(useridx:String){
+    fun getFollowerAll(useridx: String) {
 
         val tempList = mutableListOf<SubscribeDataClass>()
         val tempList2 = mutableListOf<UserDataClass>()
 
-        SubscribeRepository.getSubscribeListByUserIdx(useridx){
-            for(c1 in it.result.children){
-                val userIdx = c1.child("userIdx").value as String
+        SubscribeRepository.getSubscribeListByUserIdx(useridx) {
+            for (c1 in it.result.children) {
+                val userIdx1 = c1.child("userIdx").value as String
                 val followerIdx = c1.child("followerIdx").value as String
 
-                val s1= SubscribeDataClass(userIdx,followerIdx)
+                val s1 = SubscribeDataClass(userIdx1, followerIdx)
+                Log.d("구독", s1.toString())
                 tempList.add(s1)
+                Log.d("구독2", followerIdx)
 
-                UserRepository.getUserInfoByUserIdx(userIdx){
-                    for(c2 in it.result.children){
-                        val idx = c1.child("userIdx").value as String
-                        val email = c1.child("email").value as String
-                        val pw = c1.child("pw").value as String
-                        val nickname = c1.child("nickname").value as String
-                        val phoneNum = c1.child("phoneNum").value as String
-                        val profileImg = c1.child("profileImg").value as String
-                        val verify = c1.child("verify").value as String
+            }
+            subscribeList.value = tempList
+
+            subscribeList.value?.forEach {
+                UserRepository.getUserInfoByUserIdx(it.followerIdx) {
+                    for (c2 in it.result.children) {
+                        val idx = c2.child("idx").value as String
+                        val email = c2.child("email").value as String
+                        val pw = c2.child("pw").value as String
+                        val nickname = c2.child("nickname").value as String
+                        val phoneNum = c2.child("phoneNum").value as String
+                        val profileImg = c2.child("profileImg").value as String
+                        val verify = c2.child("verify").value as String
 
                         val u = UserDataClass(idx, email, pw, nickname, verify, phoneNum, profileImg)
-
                         tempList2.add(u)
+                        Log.d("구독3", tempList2.toString())
                     }
-
+                    followerList.value = tempList2
+                    Log.d("구독4", followerList.value.toString())
                 }
+
             }
-            followerList.value = tempList2
         }
+
     }
 
-    fun getFollowingAll(followeridx:String){
+    fun getFollowingAll(useridx: String) {
 
         val tempList = mutableListOf<SubscribeDataClass>()
         val tempList2 = mutableListOf<UserDataClass>()
 
-        SubscribeRepository.getSubscribeListByFollowerIdx(followeridx){
-            for(c1 in it.result.children){
-                val userIdx = c1.child("userIdx").value as String
+        SubscribeRepository.getSubscribeListByUserIdx(useridx) {
+            for (c1 in it.result.children) {
+                val userIdx1 = c1.child("userIdx").value as String
                 val followerIdx = c1.child("followerIdx").value as String
 
-                val s1= SubscribeDataClass(userIdx,followerIdx)
+                val s1 = SubscribeDataClass(userIdx1, followerIdx)
+                Log.d("내가", s1.toString())
                 tempList.add(s1)
+                Log.d("내가2", followerIdx)
 
-                UserRepository.getUserInfoByUserIdx(userIdx){
-                    for(c2 in it.result.children){
-                        val idx = c1.child("userIdx").value as String
-                        val email = c1.child("email").value as String
-                        val pw = c1.child("pw").value as String
-                        val nickname = c1.child("nickname").value as String
-                        val phoneNum = c1.child("phoneNum").value as String
-                        val profileImg = c1.child("profileImg").value as String
-                        val verify = c1.child("verify").value as String
+            }
+            subscribeList.value = tempList
+
+            subscribeList.value?.forEach {
+                UserRepository.getUserInfoByUserIdx(it.userIdx) {
+                    for (c2 in it.result.children) {
+                        val idx = c2.child("idx").value as String
+                        val email = c2.child("email").value as String
+                        val pw = c2.child("pw").value as String
+                        val nickname = c2.child("nickname").value as String
+                        val phoneNum = c2.child("phoneNum").value as String
+                        val profileImg = c2.child("profileImg").value as String
+                        val verify = c2.child("verify").value as String
 
                         val u = UserDataClass(idx, email, pw, nickname, verify, phoneNum, profileImg)
-
                         tempList2.add(u)
+                        Log.d("내가3", tempList2.toString())
                     }
-
+                    followingList.value = tempList2
+                    Log.d("내가4", followerList.value.toString())
                 }
+
             }
-            followingList.value = tempList2
         }
+
     }
 
 
