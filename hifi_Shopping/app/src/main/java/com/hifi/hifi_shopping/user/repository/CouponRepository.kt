290bf9dfc706bd451741af8bb03d3f96c59f87c1
iiolo <alohalo98@naver.com>
2023-08-23 15:51:43 +0900

    fix : 데이터 모델에 맞게 레포지토리 메서드 안 자료형 cast 수정

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/repository/CouponRepository.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/repository/CouponRepository.kt
@@ -6,29 +6,14 @@ import com.google.firebase.database.FirebaseDatabase
 import com.hifi.hifi_shopping.user.model.CouponDataClass
 import com.hifi.hifi_shopping.user.model.PointDataClass
 import com.hifi.hifi_shopping.user.model.UserCouponDataClass
+import com.hifi.hifi_shopping.user.model.UserDataClass
 import java.util.UUID
 
 class CouponRepository {
     companion object {
         fun addCouponData(couponData: CouponDataClass, callback1: (Task<Void>) -> Unit) {
-//        val coupon1idx = UUID.randomUUID().toString()
-//        val coupon2idx = UUID.randomUUID().toString()
-//
-//        val couponData1 = CouponDataClass(coupon1idx,"12", "2023-09-25",50,true)
-//        val couponData2 = CouponDataClass(coupon2idx,"13", "2023-09-26",35,true)
-//
-//        val userCouponData1 = UserCouponDataClass(userData.idx,coupon1idx,true)
-//        val userCouponData2 = UserCouponDataClass(userData.idx,coupon2idx, false)
-
             val database = FirebaseDatabase.getInstance()
-
             val couponDataRef = database.getReference("CouponData")
-//        couponDataRef.push().setValue(couponData1)
-//        couponDataRef.push().setValue(couponData2)
-//
-//        val userCouponDataRef = database.getReference("UserCouponData")
-//        userCouponDataRef.push().setValue(userCouponData1)
-//        userCouponDataRef.push().setValue(userCouponData2)
 
             couponDataRef.push().setValue(couponData).addOnCompleteListener(callback1)
         }
@@ -39,10 +24,11 @@ class CouponRepository {
             couponDataRef.orderByChild("validDate").get().addOnCompleteListener(callback1)
         }
 
-        fun getCouponInfo(couponIdx:String, callback1 : (Task<DataSnapshot>) -> Unit){
+        fun getCouponInfo(couponIdx: String, callback1: (Task<DataSnapshot>) -> Unit) {
             val database = FirebaseDatabase.getInstance()
             val postDataRef = database.getReference("CouponData")
-            postDataRef.orderByChild("couponIdx").equalTo(couponIdx).get().addOnCompleteListener(callback1)
+            postDataRef.orderByChild("couponIdx").equalTo(couponIdx).get()
+                .addOnCompleteListener(callback1)
         }
     }
 }
--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/repository/PointRepository.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/repository/PointRepository.kt
@@ -10,14 +10,8 @@ import com.hifi.hifi_shopping.user.model.UserDataClass
 class PointRepository {
     companion object {
         fun addPointData(pointData: PointDataClass, callback1: (Task<Void>) -> Unit) {
-//        val pointData1 = PointDataClass(userData.idx,200,"2023-08-22","회원 가입")
-//        val pointData2 = PointDataClass(userData.idx,-10,"2023-08-23","상품 구입")
-
             val database = FirebaseDatabase.getInstance()
             val pointDataRef = database.getReference("PointData")
-
-//        pointDataRef.push().setValue(pointData1)
-//        pointDataRef.push().setValue(pointData2)
 
             pointDataRef.push().setValue(pointData).addOnCompleteListener(callback1)
         }
