af68edfe0ec99880fa0d5194da0d5bc5bbf0f13e
KHSSE <113084857+KHSSE@users.noreply.github.com>
2023-08-24 10:52:49 +0900

    feat : 비번찾기 화면 (사용자 닉네임) 로직 구현 및 Dialog 추가

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/auth/AuthFindPwFragment.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/auth/AuthFindPwFragment.kt
@@ -4,7 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.Query
+import com.google.firebase.database.ValueEventListener
+import com.hifi.hifi_shopping.R
 import com.hifi.hifi_shopping.databinding.FragmentAuthFindPwBinding
 
 class AuthFindPwFragment : Fragment() {
@@ -20,22 +30,64 @@ class AuthFindPwFragment : Fragment() {
         fragmentAuthFindPwBinding = FragmentAuthFindPwBinding.inflate(inflater)
         authActivity = activity as AuthActivity
 
-        fragmentAuthFindPwBinding.run {
-            // 네비게이션 아이콘을 클릭했을 때의 동작 설정
-            toolbarAuthFindPw.setNavigationOnClickListener {
-                authActivity.removeFragment(AuthActivity.AUTH_FIND_PW_FRAGMENT)
-            }
-
-            // "다음" 버튼 클릭 시
-            buttonFindPwCheck.run{
-                setOnClickListener{
-                    authActivity.replaceFragment(AuthActivity.AUTH_FIND_RESULT_FRAGMENT, true, null)
-                }
-            }
+        fragmentAuthFindPwBinding.toolbarAuthFindPw.setNavigationOnClickListener {
+            authActivity.removeFragment(AuthActivity.AUTH_FIND_PW_FRAGMENT)
         }
 
-        return   fragmentAuthFindPwBinding.root
+        fragmentAuthFindPwBinding.buttonFindPwCheck.setOnClickListener {
+            val nickname = fragmentAuthFindPwBinding.editTextFindPwNickname.text.toString()
+            val number = fragmentAuthFindPwBinding.editTextFindPwNumber.text.toString()
+
+            checkNicknameInFirebase(nickname, number)
+        }
+
+        return fragmentAuthFindPwBinding.root
     }
 
+    private fun checkNicknameInFirebase(nickname: String, number: String) {
+        val databaseReference: DatabaseReference =
+            FirebaseDatabase.getInstance().getReference("UserData")
 
+        val query: Query = databaseReference.orderByChild("nickname").equalTo(nickname)
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // 닉네임이 파이어베이스에 등록되어 있는 경우
+                    val bundle = Bundle()
+                    bundle.putString("nickname", nickname)
+                    bundle.putString("number", number)
+
+                    val authFindResultFragment = AuthFindResultFragment()
+                    authFindResultFragment.arguments = bundle
+
+                    authActivity.replaceFragment(AuthActivity.AUTH_FIND_RESULT_FRAGMENT, true, null
+                    )
+                } else {
+                    // 닉네임이 파이어베이스에 등록되어 있지 않은 경우
+                    showNicknameNotRegisteredDialog()
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // 조회 취소 시 동작
+            }
+        })
+    }
+
+    private fun showNicknameNotRegisteredDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_nickname_not_registered, null)
+        val dialogButton = dialogView.findViewById<Button>(R.id.buttonDialogDismiss)
+
+        val alertDialogBuilder = AlertDialog.Builder(requireContext())
+            .setView(dialogView)
+            .setCancelable(false)
+
+        val alertDialog = alertDialogBuilder.create()
+        dialogButton.setOnClickListener {
+            alertDialog.dismiss()
+        }
+
+        alertDialog.show()
+    }
 }
+
