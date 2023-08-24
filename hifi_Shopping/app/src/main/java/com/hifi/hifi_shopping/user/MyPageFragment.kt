f6e8320497d1f0b0a67b513163c0a8b3563c9139
iiolo <alohalo98@naver.com>
2023-08-24 15:55:04 +0900

    feat : 배송 현황 및 기본템 현황 count 정보 연결

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/MyPageFragment.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/MyPageFragment.kt
@@ -13,7 +13,9 @@ import com.hifi.hifi_shopping.R
 import com.hifi.hifi_shopping.databinding.FragmentMyPageBinding
 import com.hifi.hifi_shopping.user.model.PointDataClass
 import com.hifi.hifi_shopping.user.model.UserDataClass
+import com.hifi.hifi_shopping.user.vm.OrderViewModel
 import com.hifi.hifi_shopping.user.vm.PointViewModel
+import com.hifi.hifi_shopping.user.vm.ProductViewModel
 import com.hifi.hifi_shopping.user.vm.ReviewViewModel
 import com.hifi.hifi_shopping.user.vm.SubscribeViewModel
 import com.hifi.hifi_shopping.user.vm.UserCouponViewModel
@@ -27,6 +29,8 @@ class MyPageFragment : Fragment() {
     lateinit var userCouponViewModel: UserCouponViewModel
     lateinit var reviewViewModel: ReviewViewModel
     lateinit var subscribeViewModel: SubscribeViewModel
+    lateinit var orderViewModel: OrderViewModel
+    lateinit var productViewModel: ProductViewModel
 
 
     override fun onCreateView(
@@ -42,6 +46,8 @@ class MyPageFragment : Fragment() {
         userCouponViewModel = ViewModelProvider(userActivity)[UserCouponViewModel::class.java]
         reviewViewModel = ViewModelProvider(userActivity)[ReviewViewModel::class.java]
         subscribeViewModel = ViewModelProvider(userActivity)[SubscribeViewModel::class.java]
+        orderViewModel = ViewModelProvider(userActivity)[OrderViewModel::class.java]
+        productViewModel = ViewModelProvider(userActivity)[ProductViewModel::class.java]
 
 
         pointViewModel.run {
@@ -74,10 +80,46 @@ class MyPageFragment : Fragment() {
             }
         }
 
+        orderViewModel.run {
+            orderDataList.observe(userActivity){
+                val orderPackingCount = orderDataList.value?.filter { it.status=="PACKING" }?.size
+                val orderShippingCount = orderDataList.value?.filter { it.status=="SHIPPING" }?.size
+                val orderArrivingCount = orderDataList.value?.filter { it.status=="ARRIVING" }?.size
+                val orderSuccessCount = orderDataList.value?.filter { it.status=="SUCCESS" }?.size
+
+                fragmentMyPageBinding.run {
+                    myPageDeliverStatusPackingCount.text = orderPackingCount.toString()
+                    myPageDeliverStatusShippingCount.text = orderShippingCount.toString()
+                    myPageDeliverStatusArrivingCount.text = orderArrivingCount.toString()
+                    myPageDeliverStatusSuccessCount.text = orderSuccessCount.toString()
+                }
+
+            }
+
+            productDataList.observe(userActivity){
+                val productBasic = productDataList.value?.map { p -> p.category.get(0)}
+                val productPrepareCount = productBasic?.filter { it.toString()=="1"}?.size
+                val productManagingCount = productBasic?.filter { it.toString()=="2"}?.size
+                val productHobbyCount = productBasic?.filter { it.toString()=="3"}?.size
+                val productDatingCount = productBasic?.filter { it.toString()=="4"}?.size
+
+                fragmentMyPageBinding.run {
+                    myPageBasicItemStatusPrepareCount.text = productPrepareCount.toString()
+                    myPageBasicItemStatusManagingCount.text = productManagingCount.toString()
+                    myPageBasicItemStatusHobbyCount.text = productHobbyCount.toString()
+                    myPageBasicItemStatusDatingCount.text = productDatingCount.toString()
+                }
+
+            }
+        }
+
+
+
         fragmentMyPageBinding.run {
             myPageToolbar.run {
                 setNavigationOnClickListener {
                     userActivity.removeFragment(UserActivity.MY_PAGE_FRAGMENT)
+
                 }
 
                 setOnMenuItemClickListener {
@@ -125,12 +167,30 @@ class MyPageFragment : Fragment() {
             }
 
             // 아이템 추천 받으러 가기
+            myPageBtnToRecommend.setOnClickListener {
+
+            }
 
 
-
-
+            orderViewModel.getOrderListByUser(userTemp.idx)
             // 배송 현황
-            myPageDeliverStatusPay.run {
+            myPageDeliverStatusPacking.run {
+
+                setOnClickListener{
+
+                }
+            }
+            myPageDeliverStatusShipping.run {
+                setOnClickListener{
+
+                }
+            }
+            myPageDeliverStatusArriving.run {
+                setOnClickListener{
+
+                }
+            }
+            myPageDeliverStatusSuccess.run {
                 setOnClickListener{
 
                 }
@@ -144,7 +204,7 @@ class MyPageFragment : Fragment() {
                 }
             }
 
-            myPageBasicItemStatusLiving.run {
+            myPageBasicItemStatusDating.run {
                 setOnClickListener{
 
                 }
@@ -156,9 +216,28 @@ class MyPageFragment : Fragment() {
                 }
             }
 
-            // 내 구독리스트 확인하러 가기
-            myPageBtnToSubscribe.setOnClickListener {
+            myPageBasicItemStatusManaging.run {
+                setOnClickListener{
 
+                }
+            }
+
+            // 내 구독리스트 확인하러 가기
+            myPageBtnToSubscribe.run {
+                setOnClickListener {
+
+                }
+            }
+
+            userBottomNavigationView.run {
+                selectedItemId = R.id.userFragment
+                setOnItemSelectedListener {
+                    when (it.itemId) {
+                        R.id.userFragment -> {
+                        }
+                    }
+                    return@setOnItemSelectedListener true
+                }
             }
         }
 
