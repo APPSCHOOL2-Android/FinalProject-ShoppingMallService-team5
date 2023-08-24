6731227b1eaef320263013bbf89baec012becd77
KHSSE <113084857+KHSSE@users.noreply.github.com>
2023-08-24 11:12:05 +0900

    Design : 비번찾기 화면 닉네임 불일치 경고 변경 (Dialog -> 텍스트 Visible)

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/auth/AuthFindPwFragment.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/auth/AuthFindPwFragment.kt
@@ -4,17 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.Query
 import com.google.firebase.database.ValueEventListener
-import com.hifi.hifi_shopping.R
 import com.hifi.hifi_shopping.databinding.FragmentAuthFindPwBinding
 
 class AuthFindPwFragment : Fragment() {
@@ -64,30 +61,20 @@ class AuthFindPwFragment : Fragment() {
                     )
                 } else {
                     // 닉네임이 파이어베이스에 등록되어 있지 않은 경우
-                    showNicknameNotRegisteredDialog()
+                    fragmentAuthFindPwBinding.warningNickname.visibility = View.VISIBLE
                 }
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
-                // 조회 취소 시 동작
+                val snackbar = Snackbar.make(
+                    fragmentAuthFindPwBinding.root,
+                    "연결에 오류가 발생했습니다.",
+                    Snackbar.LENGTH_LONG
+                )
+                snackbar.show()
             }
         })
     }
 
-    private fun showNicknameNotRegisteredDialog() {
-        val dialogView = layoutInflater.inflate(R.layout.dialog_nickname_not_registered, null)
-        val dialogButton = dialogView.findViewById<Button>(R.id.buttonDialogDismiss)
-
-        val alertDialogBuilder = AlertDialog.Builder(requireContext())
-            .setView(dialogView)
-            .setCancelable(false)
-
-        val alertDialog = alertDialogBuilder.create()
-        dialogButton.setOnClickListener {
-            alertDialog.dismiss()
-        }
-
-        alertDialog.show()
-    }
 }
 
