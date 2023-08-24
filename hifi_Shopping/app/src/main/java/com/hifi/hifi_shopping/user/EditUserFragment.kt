0dd4b1e49899bad1ce69ffb818ca2765617a5693
iiolo <alohalo98@naver.com>
2023-08-25 00:11:08 +0900

    feat : 휴대폰 본인 인증 기능 추가(인증 할 경우 비밀번호 변경 폼 나오게 설정)

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/EditUserFragment.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/EditUserFragment.kt
@@ -1,25 +1,47 @@
 package com.hifi.hifi_shopping.user
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.hifi.hifi_shopping.R
 import com.hifi.hifi_shopping.databinding.FragmentEditUserBinding
 import com.hifi.hifi_shopping.search.SearchActivity
+import com.hifi.hifi_shopping.user.vm.AddressViewModel
+import com.hifi.hifi_shopping.user.vm.ProductViewModel
 
 class EditUserFragment : Fragment() {
 
     lateinit var fragmentEditUserBinding: FragmentEditUserBinding
     lateinit var userActivity : UserActivity
+    lateinit var addressViewModel: AddressViewModel
+    var isToggled = false
+    var isVerify = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         fragmentEditUserBinding = FragmentEditUserBinding.inflate(layoutInflater)
         userActivity = activity as UserActivity
+
+        val userTemp = userActivity.userTemp
+
+        addressViewModel = ViewModelProvider(userActivity)[AddressViewModel::class.java]
+
+        addressViewModel.run {
+            addressDataList.observe(userActivity){
+                val addr = addressDataList.value?.get(0)?.address?.split("/")
+                fragmentEditUserBinding.editUserAddrEditText.setText(addr?.get(0))
+                fragmentEditUserBinding.editUserAddrDetailEditText.setText(addr?.get(1))
+            }
+        }
 
         fragmentEditUserBinding.run {
             editUserToolbar.run {
@@ -42,7 +64,87 @@ class EditUserFragment : Fragment() {
                 }
             }
 
+            editUserEditNick.setText(userTemp.nickname)
+            addressViewModel.getAddressListByUser(userTemp.idx)
+
+            editUserToChangePwdBtnToggle.setOnClickListener {
+                toggleButtonClick()
+            }
+
+            editUserPhoneBtnCheck.setOnClickListener {
+                phoneCheck()
+
+            }
+            editUserPhoneBtnCheckVerify.setOnClickListener {
+                phoneVerify()
+            }
+
+
         }
         return fragmentEditUserBinding.root
     }
+
+    fun toggleButtonClick() {
+        isToggled = !isToggled
+        if (isToggled) {
+            // 토글이 활성화된 경우
+            fragmentEditUserBinding.run {
+                editUserToChangePwdBtnToggle.setImageResource(R.drawable.expand_less_24px)
+                editUserPhone.visibility = View.VISIBLE
+                if(isVerify){
+                    editUserPwdEditTextChange.visibility = View.VISIBLE
+                }
+            }
+        } else {
+            // 토글이 비활성화된 경우
+            fragmentEditUserBinding.run {
+                editUserToChangePwdBtnToggle.setImageResource(R.drawable.expand_more_24px)
+                editUserPhone.visibility = View.GONE
+                if(isVerify){
+                    editUserPwdEditTextChange.visibility = View.GONE
+                }
+            }
+        }
+    }
+
+    fun phoneCheck() {
+        fragmentEditUserBinding.run {
+            if(editUserPhoneEditText.text.toString().isEmpty()){
+                val builder = MaterialAlertDialogBuilder(userActivity)
+                builder.setTitle("전화번호 오류")
+                builder.setMessage("전화번호를 입력해주세요.")
+                builder.setPositiveButton("확인"){ dialogInterface: DialogInterface, i: Int ->
+                    userActivity.showSoftInput(editUserPhoneEditText)
+                }
+                builder.show()
+                return
+            }else {
+                userActivity.hideKeyboard(editUserPhoneEditText)
+                editUserPhoneEditTextVerify.setText("1312")
+            }
+
+        }
+
+    }
+
+    fun phoneVerify(){
+        fragmentEditUserBinding.run {
+            val verifyText = editUserPhoneEditTextVerify.text.toString()
+            Log.d("휴대폰",verifyText.toString())
+            if(verifyText == "1312"){
+                isVerify=true
+                editUserPwdEditTextChange.visibility = View.VISIBLE
+            }
+            if(isVerify){
+                editUserPhoneEditText.isEnabled = false
+                editUserPhoneBtnCheck.text = "인증 완료"
+                editUserPhoneBtnCheck.isClickable = false
+                userActivity.hideKeyboard(editUserPhoneEditText)
+
+                editUserPhoneEditTextVerify.visibility = View.GONE
+                editUserPhoneBtnCheckVerify.visibility = View.GONE
+            }
+        }
+    }
+
 }
