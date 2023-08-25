3fac658151e154ad712a3de2e1d368ae765d349b
iiolo <alohalo98@naver.com>
2023-08-25 16:09:22 +0900

    feat : 필요한 권한 리스트 작성

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/UserActivity.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/UserActivity.kt
@@ -1,7 +1,11 @@
 package com.hifi.hifi_shopping.user
 
+import android.Manifest
+import android.content.Context
 import android.os.Bundle
 import android.os.SystemClock
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -12,6 +16,7 @@ import com.google.android.material.transition.MaterialSharedAxis
 import com.hifi.hifi_shopping.R
 import com.hifi.hifi_shopping.databinding.ActivityUserBinding
 import com.hifi.hifi_shopping.user.model.UserDataClass
+import kotlin.concurrent.thread
 
 class UserActivity : AppCompatActivity() {
 
@@ -20,8 +25,13 @@ class UserActivity : AppCompatActivity() {
     var newFragment:Fragment? = null
     var oldFragment:Fragment? = null
 
-    var userTemp = UserDataClass("e8fa83ce-5341-4f10-9929-5521d9c5fe82", "ohsso98@naver.com", "0618", "김대박", "true", "010-1111-1111", "sample_img")
+    var userTemp = UserDataClass("e8fa83ce-5341-4f10-9929-5521d9c5fe82", "ohsso98@naver.com", "0618", "김대박", "true", "010-1111-1111", "")
 
+    val permissionList = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.ACCESS_MEDIA_LOCATION,
+        Manifest.permission.INTERNET
+    )
 
     companion object{
         val MY_PAGE_FRAGMENT = "MyPageFragment"
@@ -37,7 +47,10 @@ class UserActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         activityUserBinding = ActivityUserBinding.inflate(layoutInflater)
         setContentView(activityUserBinding.root)
+        requestPermissions(permissionList,0)
+
         replaceFragment(MY_PAGE_FRAGMENT, false, null)
+//        replaceFragment(EDIT_USER_FRAGMENT, false, null)
     }
 
     // 지정한 Fragment를 보여주는 메서드
@@ -99,5 +112,20 @@ class UserActivity : AppCompatActivity() {
     fun removeFragment(name:String){
         supportFragmentManager.popBackStack(name, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
+    fun showSoftInput(view: View){
+        view.requestFocus()
+
+        val inputMethodManger = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        thread {
+            SystemClock.sleep(200)
+            inputMethodManger.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+        }
+    }
+
+    fun hideKeyboard(view: View) {
+        val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
 
 }
