608d2010fbb3b5ee9d4b4051f8b8db46309cdd2c
iiolo <alohalo98@naver.com>
2023-08-26 00:14:56 +0900

    feat : productImg 데이터 클래스 생성

---
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/model/ProductImgDataClass.kt
@@ -0,0 +1,8 @@
+package com.hifi.hifi_shopping.user.model
+
+data class ProductImgDataClass(
+    val productIdx : String,
+    val imgOrder : String,
+    val imgSrc : String,
+    val default : String
+    )
