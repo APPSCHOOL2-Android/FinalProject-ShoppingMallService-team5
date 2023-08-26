06cfab4a5aa9b97fc602fdbaafdfe6454f97ecb2
iiolo <alohalo98@naver.com>
2023-08-27 00:52:20 +0900

    feat : productViewModel 추가

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/vm/ProductViewModel.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/vm/ProductViewModel.kt
@@ -1,21 +1,9 @@
 package com.hifi.hifi_shopping.user.vm
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.hifi.hifi_shopping.user.model.ProductDataClass
-import com.hifi.hifi_shopping.user.model.TempData
-import com.hifi.hifi_shopping.user.repository.ProductImgRepository
 import com.hifi.hifi_shopping.user.repository.ProductRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.net.HttpURLConnection
-import java.net.URL
-import kotlin.concurrent.thread
 
 class ProductViewModel : ViewModel() {
     var productDataList = MutableLiveData<MutableList<ProductDataClass>>()
