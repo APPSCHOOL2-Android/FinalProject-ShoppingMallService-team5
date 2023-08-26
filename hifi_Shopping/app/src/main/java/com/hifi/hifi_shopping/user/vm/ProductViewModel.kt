2a799b2ea3feba6f55de97fe8d13273253f52af8
iiolo <alohalo98@naver.com>
2023-08-27 00:49:12 +0900

    feat : prodcut 제품 한 개에 대한 정보만 가져오는 메서드 추가

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/vm/ProductViewModel.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/vm/ProductViewModel.kt
@@ -1,25 +1,32 @@
 package com.hifi.hifi_shopping.user.vm
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.hifi.hifi_shopping.user.model.AddressDataClass
-import com.hifi.hifi_shopping.user.model.OrderDataClass
+import androidx.lifecycle.viewModelScope
 import com.hifi.hifi_shopping.user.model.ProductDataClass
-import com.hifi.hifi_shopping.user.model.ReviewDataClass
-import com.hifi.hifi_shopping.user.repository.AddressRepository
-import com.hifi.hifi_shopping.user.repository.OrderRepository
+import com.hifi.hifi_shopping.user.model.TempData
+import com.hifi.hifi_shopping.user.repository.ProductImgRepository
 import com.hifi.hifi_shopping.user.repository.ProductRepository
-import com.hifi.hifi_shopping.user.repository.ReviewRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class ProductViewModel : ViewModel() {
-
     var productDataList = MutableLiveData<MutableList<ProductDataClass>>()
+    var productDataFiveList = MutableLiveData<MutableList<ProductDataClass>>()
+    var productDataByIdx = MutableLiveData<ProductDataClass>()
 
-    fun getProductListByUser(userIdx : String){
+    fun getProductList() {
 
         val tempList = mutableListOf<ProductDataClass>()
 
-        ProductRepository.getProductInfoByIdx(userIdx) {
+        ProductRepository.getProductAll {
             for (c1 in it.result.children) {
                 val category = c1.child("category").value as String
                 val context = c1.child("context").value as String
@@ -29,10 +36,34 @@ class ProductViewModel : ViewModel() {
                 val price = c1.child("price").value as String
                 val sellerIdx = c1.child("sellerIdx").value as String
 
-                val data = ProductDataClass(category, context, idx, name, pointAmount, price, sellerIdx)
+                val data =
+                    ProductDataClass(category, context, idx, name, pointAmount, price, sellerIdx)
                 tempList.add(data)
             }
             productDataList.value = tempList
         }
     }
+
+    fun getProductInfoByIdx(idx: String) {
+
+        ProductRepository.getProductInfoByIdx(idx) {
+            for (c1 in it.result.children) {
+                val category = c1.child("category").value as String
+                val context = c1.child("context").value as String
+                val idx = c1.child("idx").value as String
+                val name = c1.child("name").value as String
+                val pointAmount = c1.child("pointAmount").value as String
+                val price = c1.child("price").value as String
+                val sellerIdx = c1.child("sellerIdx").value as String
+
+                val data =
+                    ProductDataClass(category, context, idx, name, pointAmount, price, sellerIdx)
+                productDataByIdx.value = data
+            }
+
+        }
+    }
 }
+
+
+
