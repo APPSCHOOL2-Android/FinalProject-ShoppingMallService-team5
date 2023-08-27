e052eaaca1c7f9d1c03fe98fbb6692199c08f350
iiolo <alohalo98@naver.com>
2023-08-28 02:56:03 +0900

    feat : 리뷰 기다리고 있어요 항목들 정보 연결 및 클릭 이벤트 연결

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/PointFragment.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/PointFragment.kt
@@ -1,28 +1,44 @@
 package com.hifi.hifi_shopping.user
 
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.storage.FirebaseStorage
 import com.hifi.hifi_shopping.R
-import com.hifi.hifi_shopping.category.CategoryActivity
+import com.hifi.hifi_shopping.buy.BuyActivity
 import com.hifi.hifi_shopping.databinding.FragmentPointBinding
 import com.hifi.hifi_shopping.databinding.RowPointBinding
+import com.hifi.hifi_shopping.databinding.RowPointReviewBinding
+import com.hifi.hifi_shopping.review.ReviewActivity
 import com.hifi.hifi_shopping.search.SearchActivity
+import com.hifi.hifi_shopping.user.repository.ProductImgRepository
+import com.hifi.hifi_shopping.user.repository.ProductRepository
+import com.hifi.hifi_shopping.user.vm.OrderViewModel
 import com.hifi.hifi_shopping.user.vm.PointViewModel
+import com.hifi.hifi_shopping.user.vm.ReviewViewModel
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class PointFragment : Fragment() {
     lateinit var fragmentPointBinding: FragmentPointBinding
     lateinit var userActivity: UserActivity
     lateinit var pointViewModel: PointViewModel
+    lateinit var orderViewModel: OrderViewModel
+    lateinit var reviewViewModel: ReviewViewModel
     var isToggled = false
+
+    private val rowPointReviewBindingList = mutableListOf<RowPointReviewBinding>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,8 +46,14 @@ class PointFragment : Fragment() {
         fragmentPointBinding = FragmentPointBinding.inflate(layoutInflater)
         userActivity = activity as UserActivity
         pointViewModel = ViewModelProvider(userActivity)[PointViewModel::class.java]
+        orderViewModel = ViewModelProvider(userActivity)[OrderViewModel::class.java]
+        reviewViewModel = ViewModelProvider(userActivity)[ReviewViewModel::class.java]
+
         val userTemp = userActivity.userTemp
 
+        reviewViewModel.run{
+             getReviewListByUser(userTemp.idx)
+        }
         pointViewModel.run {
             getPointListByUser(userTemp.idx)
             pointDataList.observe(userActivity) {
@@ -42,6 +64,49 @@ class PointFragment : Fragment() {
                         adapter = PointRecyclerViewAdapter()
                         layoutManager =
                             LinearLayoutManager(userActivity, LinearLayoutManager.VERTICAL, false)
+                    }
+                }
+            }
+        }
+        
+        orderViewModel.run { 
+            getOrderListByUser(userTemp.idx)
+            orderDataList.observe(userActivity){
+                fragmentPointBinding.run { 
+                    pointReviewLayout.removeAllViews()
+                    orderDataList.value?.forEach {
+                        val productidx = it.productIdx
+                        val foundItem = reviewViewModel.reviewDataList.value?.firstOrNull { it.productIdx == productidx }
+                        if(foundItem==null) {
+                            val rowPointReviewBinding =
+                                RowPointReviewBinding.inflate(LayoutInflater.from(userActivity))
+                            // 데이터 설정
+                            rowPointReviewBinding.run {
+                                rowPointReviewDate.text = it.date + " 결제"
+                                rowPointReviewIdx.text = it.productIdx
+                                ProductRepository.getProductInfoByIdx(it.productIdx) {
+                                    for (c1 in it.result.children) {
+                                        rowPointReviewName.text = c1.child("name").value as String
+                                    }
+                                }
+
+                                rowPointReviewPrice.text = it.price + "원"
+                                // 제품 이미지
+                                getProductImg(it.idx, rowPointReviewImg)
+                                rowPointReviewImg.setOnClickListener {
+                                    val intent = Intent(userActivity, BuyActivity::class.java)
+                                    intent.putExtra("productIdx", rowPointReviewIdx.text.toString())
+                                    startActivity(intent)
+                                }
+                                rowPointReviewBtnReview.setOnClickListener {
+                                    val intent = Intent(userActivity, ReviewActivity::class.java)
+                                    intent.putExtra("productIdx", rowPointReviewIdx.text.toString())
+                                    startActivity(intent)
+                                }
+                            }
+                            pointReviewLayout.addView(rowPointReviewBinding.root)
+                            rowPointReviewBindingList.add(rowPointReviewBinding)
+                        }
                     }
                 }
             }
@@ -136,5 +201,33 @@ class PointFragment : Fragment() {
         }
     }
 
+    fun getProductImg(productidx: String, imgView: ImageView) {
+        ProductImgRepository.getProductImgInfoByProductIdx(productidx) {
+            for (c1 in it.result.children) {
+                val imgSrc = c1.child("imgSrc").value as String
+                val default = c1.child("default").value as String
+
+                if (default == "true") {
+                    val storage = FirebaseStorage.getInstance()
+                    val fileRef = storage.reference.child("product/${imgSrc}")
+
+                    // 데이터를 가져올 수 있는 경로를 가져온다.
+                    fileRef.downloadUrl.addOnCompleteListener {
+                        thread {
+                            // 파일에 접근할 수 있는 경로를 이용해 URL 객체를 생성한다.
+                            val url = URL(it.result.toString())
+                            // 접속한다.
+                            val httpURLConnection = url.openConnection() as HttpURLConnection
+                            // 이미지 객체를 생성한다.
+                            val bitmap = BitmapFactory.decodeStream(httpURLConnection.inputStream)
+                            userActivity.runOnUiThread {
+                                imgView.setImageBitmap(bitmap)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 }
