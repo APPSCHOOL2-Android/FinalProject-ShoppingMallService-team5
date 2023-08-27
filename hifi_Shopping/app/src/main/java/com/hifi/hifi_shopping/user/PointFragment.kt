e2fd9d76a4d6df6a94b7be26f2531cd3ec2f899c
iiolo <alohalo98@naver.com>
2023-08-28 02:26:31 +0900

    feat : toggle 버튼 이벤트 추가

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/PointFragment.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/user/PointFragment.kt
@@ -1,33 +1,25 @@
 package com.hifi.hifi_shopping.user
 
-import android.content.Intent
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.storage.FirebaseStorage
 import com.hifi.hifi_shopping.R
-import com.hifi.hifi_shopping.buy.BuyActivity
 import com.hifi.hifi_shopping.databinding.FragmentPointBinding
 import com.hifi.hifi_shopping.databinding.RowPointBinding
-import com.hifi.hifi_shopping.user.repository.ProductImgRepository
-import com.hifi.hifi_shopping.user.vm.OrderViewModel
 import com.hifi.hifi_shopping.user.vm.PointViewModel
-import java.net.HttpURLConnection
-import java.net.URL
-import kotlin.concurrent.thread
 
 class PointFragment : Fragment() {
     lateinit var fragmentPointBinding: FragmentPointBinding
     lateinit var userActivity: UserActivity
     lateinit var pointViewModel: PointViewModel
+    var isToggled = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,33 +28,47 @@ class PointFragment : Fragment() {
         userActivity = activity as UserActivity
         pointViewModel = ViewModelProvider(userActivity)[PointViewModel::class.java]
         val userTemp = userActivity.userTemp
+
         pointViewModel.run {
             getPointListByUser(userTemp.idx)
-            pointDataList.observe(userActivity){
+            pointDataList.observe(userActivity) {
                 fragmentPointBinding.run {
-                    pointUserPoint.text = pointDataList.value?.map { it.amount.toInt() }?.sum().toString() + "P"
+                    pointUserPoint.text =
+                        pointDataList.value?.map { it.amount.toInt() }?.sum().toString() + "P"
                     pointDetailRecyclerView.run {
                         adapter = PointRecyclerViewAdapter()
-                        layoutManager = LinearLayoutManager(userActivity, LinearLayoutManager.VERTICAL, false)
+                        layoutManager =
+                            LinearLayoutManager(userActivity, LinearLayoutManager.VERTICAL, false)
                     }
                 }
             }
         }
 
-        
+        fragmentPointBinding.run {
+            pointGuideBtnToggle.setOnClickListener {
+                toggleButtonClick(pointGuideBtnToggle,pointGuideTextView)
+            }
+            pointReviewBtnToggle.setOnClickListener {
+                toggleButtonClick(pointReviewBtnToggle,pointReviewLayout)
+            }
+        }
+
+
         return fragmentPointBinding.root
     }
 
-    inner class PointRecyclerViewAdapter() : RecyclerView.Adapter<PointRecyclerViewAdapter.PointRecyclerViewHolder>(){
+    inner class PointRecyclerViewAdapter() :
+        RecyclerView.Adapter<PointRecyclerViewAdapter.PointRecyclerViewHolder>() {
 
-        inner class PointRecyclerViewHolder(rowPointBinding: RowPointBinding) : RecyclerView.ViewHolder(rowPointBinding.root){
+        inner class PointRecyclerViewHolder(rowPointBinding: RowPointBinding) :
+            RecyclerView.ViewHolder(rowPointBinding.root) {
 
             val rowPointDate: TextView
             val rowPointContext: TextView
             val rowPointAmount: TextView
 
 
-            init{
+            init {
                 rowPointDate = rowPointBinding.rowPointDate
                 rowPointContext = rowPointBinding.rowPointContext
                 rowPointAmount = rowPointBinding.rowPointAmount
@@ -88,8 +94,24 @@ class PointFragment : Fragment() {
         override fun onBindViewHolder(holder: PointRecyclerViewHolder, position: Int) {
             holder.rowPointDate.text = pointViewModel.pointDataList.value?.get(position)?.date
             holder.rowPointContext.text = pointViewModel.pointDataList.value?.get(position)?.context
-            holder.rowPointAmount.text = pointViewModel.pointDataList.value?.get(position)?.amount+"P"
+            holder.rowPointAmount.text =
+                pointViewModel.pointDataList.value?.get(position)?.amount + "P"
 
         }
     }
+
+    fun toggleButtonClick(btn: ImageButton, view: View) {
+        isToggled = !isToggled
+        if (isToggled) {
+            // 토글이 활성화된 경우
+            btn.setImageResource(R.drawable.expand_less_24px)
+            view.visibility = View.VISIBLE
+        } else {
+            btn.setImageResource(R.drawable.expand_more_24px)
+            view.visibility = View.GONE
+
+        }
+    }
+
+
 }
