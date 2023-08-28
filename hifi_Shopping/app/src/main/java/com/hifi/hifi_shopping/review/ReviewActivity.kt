f9baa7aef882ce69ea416bf0fa702a12eb20d5bd
jlkim909 <jlkim909@gmail.com>
2023-08-28 09:42:45 +0900

    fix : review 화면 Fragment -> Activity로 변경

---
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/review/ReviewActivity.kt
@@ -0,0 +1,12 @@
+package com.hifi.hifi_shopping.review
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hifi.hifi_shopping.R
+
+class ReviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review)
+    }
+}
