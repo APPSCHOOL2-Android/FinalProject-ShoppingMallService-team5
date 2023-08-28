1359b33c1a817f8014e6fd6227dca4d503ca4df7
iiolo <alohalo98@naver.com>
2023-08-28 23:23:00 +0900

    feat : 위시리스트 페이지 하단 바 클릭 이벤트 연결

--- hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/wish/WishFragment.kt
+++ hifi_Shopping/app/src/main/java/com/hifi/hifi_shopping/wish/WishFragment.kt
@@ -1,19 +1,59 @@
 package com.hifi.hifi_shopping.wish
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.hifi.hifi_shopping.R
+import com.hifi.hifi_shopping.category.CategoryActivity
+import com.hifi.hifi_shopping.databinding.FragmentWishBinding
+import com.hifi.hifi_shopping.rank.RankActivity
+import com.hifi.hifi_shopping.recommend.RecommendActivity
+import com.hifi.hifi_shopping.user.UserActivity
 
 class WishFragment : Fragment() {
+    lateinit var fragmentWishBinding : FragmentWishBinding
+    lateinit var wishActivity: WishActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wish, container, false)
+        fragmentWishBinding = FragmentWishBinding.inflate(layoutInflater)
+        wishActivity = activity as WishActivity
+
+        fragmentWishBinding.run {
+            wishBottomNavigationView.run {
+                selectedItemId = R.id.bottomMenuItemWish
+                setOnItemSelectedListener {
+                    when (it.itemId) {
+                        R.id.bottomMenuItemRankMain ->{
+                            val intent = Intent(wishActivity, RankActivity::class.java)
+                            startActivity(intent)
+
+                        }
+                        R.id.bottomMenuItemCategoryMain->{
+                            val intent = Intent(wishActivity, CategoryActivity::class.java)
+                            startActivity(intent)
+
+                        }
+                        R.id.bottomMenuItemRecommend ->{
+                            val intent = Intent(wishActivity, RecommendActivity::class.java)
+                            startActivity(intent)
+                        }
+                        R.id.bottomMenuItemMyPage ->{
+                            val intent = Intent(wishActivity, UserActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
+                    return@setOnItemSelectedListener true
+                }
+            }
+
+        }
+
+        return fragmentWishBinding.root
     }
 }
